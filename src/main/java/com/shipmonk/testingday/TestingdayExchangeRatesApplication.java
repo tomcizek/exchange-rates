25fde64b25e1b5516fedcb5ddfcbc4db2a018199
Filip Procházka <filip.prochazka@shipmonk.com>
2022-06-21 17:17:38 +0200

    start.spring.io

---
+++ src/main/java/com/shipmonk/testingday/TestingdayExchangeRatesApplication.java
@@ -0,0 +1,15 @@
+package com.shipmonk.testingday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingdayExchangeRatesApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TestingdayExchangeRatesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shipmonk/testingday/TestingdayExchangeRatesApplicationTests.java
@@ -0,0 +1,15 @@
+package com.shipmonk.testingday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingdayExchangeRatesApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
